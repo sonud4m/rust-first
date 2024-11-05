dc3b866e1169c5d41b8e901afb203f77118315a1
sonud4m <endenn209@gmail.com>
2024-11-05 21:30:45 +0900

    complete

--- src/bin/fizzbuzz.rs
+++ src/bin/fizzbuzz.rs
@@ -1,6 +1,12 @@
 fn main(){
-    let n: i32 = 20;
+    let n: usize =20;
+    println!("FizzBuzz");
+    fizz_buzz(n);
+    println!("FizzBuzzMatch");
+    fizz_buzz_match(n);
+}
 
+fn fizz_buzz(n: usize){
     for i in 1..=n {
         if i % 15 == 0 {
             println!("{} : FizzBuzz", i);
@@ -10,6 +16,17 @@ fn main(){
             println!("{} : Buzz", i);
         } else {
             println!("{}", i);
+        }
+    }
+}
+
+fn fizz_buzz_match(n: usize){
+    for i in 1..=n{
+        match(i % 3, i % 5){
+            (0, 0) => println!("{} : FizzBuzz", i),
+            (0, _) => println!("{} : Fizz", i),
+            (_, 0) => println!("{} : Buzz", i),
+            _ => println!("{}", i)
         }
     }
 }
